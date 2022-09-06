c90ab02cc09303ff4a0f7886284cb389883ae3ec
Rafael <rafaelmreyes@hotmail.com>
2022-09-06 11:55:51 -0700

    updating account.html, sending data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
-<<<<<<< HEAD
 
 console.log('hola')
 
 
 // url del backend
-const api_url = 'https://tempec-cloud-rafamr-git-main-rafamreyes17-gmailcom.vercel.app/enterprise'
+const api_url = 'https://tempec.vercel.app/'
 
+// nodos
+
+    // nodos logIn
+const signInButton = document.getElementById('signInButton')
+const loginUser = document.getElementById('user')
+const loginPassword = document.getElementById('password')
+
+    // nodos account
+const accountEnterprise = document.getElementById('accountEnterprise')
+const accountName = document.getElementById('accountName')
+const accountUser = document.getElementById('accountUser')
+const accountPassword = document.getElementById('accountPassword')
+const accountEmail = document.getElementById('accountEmail')
+const accountPhone = document.getElementById('accountPhone')
+const accountSendButton = document.getElementById('accountSendButton')
+
+// buttons
+accountSendButton.addEventListener('click', () => {
+    console.log('Sending Data')
+    createEnterprise()
+})
+
+// signInButton.addEventListener('click', () => {
+//     console.log('lo logre')
+//     login()
+// })
+
+// functions
+
+async function createEnterprise(){
+    console.log(accountName.value)
+    const res = await fetch(api_url + 'enterprise/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "enterprise": accountEnterprise.value,
+            "name": accountName.value,
+            "user": accountUser.value,
+            "password": accountPassword.value,
+            "email": accountEmail.value,
+            "phone": accountPhone.value,
+        })
+    })
+}
 
 async function login() {
-    
-    
-
+    console.log(user.value)
     console.log('access')
     // const res = await fetch(api_url)
     const res = await fetch(api_url, {
@@ -22,9 +65,9 @@ async function login() {
         },
         // se envia el body convertido en json por que no sabemos con que lenguaje fue escrito el backend
         body: JSON.stringify({
-                "name": "Esteban",
+                "name": user.value,
                 "userName": "raf",
-                "password": "123456j"
+                "password": password.value
         })
     })
     const data = await res.json()
@@ -34,57 +77,10 @@ async function login() {
         console.log('error de conexion')
     }else {
         // console.log(data)
-=======
-console.log('hola')
-
-// url del backend
-const api_url = 'https://pokeapi.co/api/v2/pokemon/'
-
-    // seleccionando el id del index.html
-    const signInButton = document.getElementById('signInButton')
-    const userButton = document.getElementById('user')
-        
-    signInButton.addEventListener('click', () => {
-        console.log('hola2')
-        let result = randomPokemon()
-        loadRandom(result)
-        console.log(result)
-    })
-
-
-const randomPokemon = () => {
-        return Math.floor((Math.random() * 3) + 1
-        ) 
-       }
-    
-    async function loadRandom(result) {
-        
-
-    const res = await fetch(`${api_url}${result}`)
-    const data = await res.json()
-    console.log('access')
-    console.log(userButton.id)
-    console.log(userButton.value)
-    console.log(data)
-    
-    if(res.status !== 200){
-        console.log('error de conexion')
-    }else {
->>>>>>> fb8d0ad9ffddaccc17372cee270eee60cf5c89d3
         return data
     }
 }
 
-<<<<<<< HEAD
-login()
-
-const signInButton = document.getElementById('signInButton')
-signInButton.addEventListener('click', login)
-=======
+// login()
 
 
-
-
-
-
->>>>>>> fb8d0ad9ffddaccc17372cee270eee60cf5c89d3
