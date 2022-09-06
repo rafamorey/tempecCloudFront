09fe7516cf505a099deec7223cbb2177ad8d01b9
Rafa morey <rafaelmreyes@hotmail.com>
2022-09-06 09:46:05 -0700

    deploy vercel

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
-<<<<<<< HEAD
 
-addEventListener
-=======
 console.log('hola')
 
+
 // url del backend
-const api_url = 'http://localhost:3002/enterprise'
+const api_url = 'https://tempec-cloud-rafamr-git-main-rafamreyes17-gmailcom.vercel.app/enterprise'
 
 
 async function login() {
     
-    const res = await fetch(api_url)
-    const data = await res.json()
+    
+
     console.log('access')
+    // const res = await fetch(api_url)
+    const res = await fetch(api_url, {
+        // especificamos el metodo de la peticion
+        method: 'POST',
+        // se envia el header para indicar que es un json
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // se envia el body convertido en json por que no sabemos con que lenguaje fue escrito el backend
+        body: JSON.stringify({
+                "name": "Esteban",
+                "userName": "raf",
+                "password": "123456j"
+        })
+    })
+    const data = await res.json()
     console.log(data)
     
-    if(res.status !== 200){
+    if(res.status !== 201){
         console.log('error de conexion')
     }else {
+        // console.log(data)
         return data
     }
 }
 
+login()
+
 const signInButton = document.getElementById('signInButton')
-signInButton.onclick = login
->>>>>>> 752e67cb5b7f99386902cb6ee203639442a45b66
+signInButton.addEventListener('click', login)
