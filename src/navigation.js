f1da9d31ab0146df992f860f6ae004b705f256a6
Rafael <rafaelmreyes@hotmail.com>
2022-09-22 20:54:44 -0700

    updating dashboard.html

--- src/navigation.js
+++ src/navigation.js
@@ -1,1 +1,8 @@
- 
+//  navigation trow the app
+
+signInButton.addEventListener('click', () => {
+    location.hash = '#dashboard'
+})
+
+window.addEventListener('DOMContentLoaded', navigator, false)
+window.addEventListener('hashchange', navigator, false)
