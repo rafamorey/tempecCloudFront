d5415bbae7bb9971de490faebd655588864ce749
Rafael <rafaelmreyes@hotmail.com>
2022-10-15 08:28:13 -0700

    adjusting account.html

--- public/src/account.js
+++ public/src/account.js
@@ -30,30 +30,53 @@ signInButton.addEventListener('click', () => {
 
 async function createEnterprise(){
     console.log("accessing")
-    const res = await fetch(api_UrlEnterprise , {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "enterprise": accountEnterprise.value,
-            "name": accountName.value,
-            "user": accountUser.value,
-            "password": accountPassword.value,
-            "email": accountEmail.value,
-            "phone": accountPhone.value,
-        })
-        
-    })
-        const data = await res.json()
-        console.log(data)
-        
-        if(res.status !== 201){
-            console.log('error de conexion')
-        }else {
-            // console.log(data)
-            return data
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/
+    if(accountEnterprise.value && accountPassword.value){
+        if(re.exec(accountPassword.value) ){
+            const res = await fetch(api_UrlEnterprise , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "enterprise": accountEnterprise.value,
+                    "name": accountName.value,
+                    "user": accountUser.value,
+                    "password": accountPassword.value,
+                    "email": accountEmail.value,
+                    "phone": accountPhone.value,
+                })
+                
+            })
+                const data = await res.json()
+                console.log(data)
+                
+                if(res.status !== 201){
+                    console.log('error de conexion')
+                }else {
+                    // console.log(data)
+                    window.location.href = 'http://127.0.0.1:5501/public/index.html'
+                    return data
+
+                }
+    
+        }else{
+            console.log("falta algun campo")
+            const errorUserInfo = document.getElementById('errorUserInfo')
+            errorUserInfo.classList.add('errorUserInfo')
+            const pErrorText = document.createTextNode('Contraseña debe incluir al menos 1 minuscula, 1 mayuscula, 1 numero y 1 caracter')
+            errorUserInfo.innerHTML = ""
+            errorUserInfo.appendChild(pErrorText)
         }
+    }else{
+        console.log("falta algun campo")
+        const errorUserField = document.getElementById('errorUserInfo')
+        errorUserField.classList.add('errorUserInfo')
+        const pErrorFieldText = document.createTextNode('Los campos de Enterprise y Password son requeridos')
+        errorUserField.innerHTML = ""
+        errorUserField.appendChild(pErrorFieldText)
+    }
+
 }
 
 
